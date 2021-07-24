d8ce9550a0807cba411d0e19b60a4c9c520d0fb9
Justin Doody <justin@20dots.com>
2021-07-24 13:12:10 -0400

    save message for post content

--- app/javascript/controllers/post_controller.js
+++ app/javascript/controllers/post_controller.js
@@ -3,7 +3,8 @@ import debounce from 'helpers/debounce'
 import Rails from 'rails-ujs';
 
 export default class extends Controller {
-    static targets = [ "form" ]
+    static targets = [ "form", "saveMessage" ]
+    static classes = [ "fadeIn", "fadeOut" ]
 
     initialize() {
         this.saveMe = debounce(this.saveMe, 750).bind(this);
@@ -12,6 +13,14 @@ export default class extends Controller {
     saveMe(event) {
         // Triggering via a .submit() on the form won't
         // respect remote: true
-        Rails.fire(this.formTarget, 'submit')
+        Rails.fire(this.formTarget, 'submit');
+
+        this.saveMessageTarget.classList.remove(this.fadeOutClass);
+        this.saveMessageTarget.classList.add(this.fadeInClass);
+
+        setTimeout(() => {
+            this.saveMessageTarget.classList.remove(this.fadeInClass);
+            this.saveMessageTarget.classList.add(this.fadeOutClass);
+        }, 2000);
     }
 }
